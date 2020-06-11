a7d0616eaa0d24432263f974af30b21ee6c2bebb
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-11 00:40:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,10 @@
 class Triangle
-  def initalize()
+  def initalize(side1, side2, side3)
+    @side1 = side1 
+    @side2 = side2 
+    @side3 = side3
+  end 
+  
+  class TriangleError < StandardError 
+  end 
 end
