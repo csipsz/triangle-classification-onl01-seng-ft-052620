2683d64c3603276c473010df7b2961a39045668b
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-11 01:06:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,4 @@
-=begin
+
 class Triangle
   
   attr_accessor :side1, :side2, :side3
@@ -36,44 +36,4 @@ class Triangle
   end 
 end
 
-=end 
 
-class Triangle
-  attr_accessor :x, :y, :z
-  def initialize(x, y, z)
-    @x = x
-    @y = y
-    @z = z
-  end
-
-  def kind
-    if kosher_triangle == true && kosher_triangle2 == true 
-      puts true 
-    else 
-      raise TriangleError 
-    end 
-    
-    if x == y && y == z && x == z
-      :equilateral
-    elsif x != y && y != z && z != x
-      :scalene 
-    else 
-      :isosceles
-    end 
-  end
-  
-  def kosher_triangle 
-    x.positive? && y.positive? && z.positive?
-  end 
-  
-  def kosher_triangle2 
-    x + y > z && x + z > y && y + z > x
-  end 
-end
-
-class TriangleError < StandardError
-  
-end
-
-a = Triangle.new(5, 5, 5)
-a.kind
