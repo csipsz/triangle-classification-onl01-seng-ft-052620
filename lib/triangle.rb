685351a40498818187b71cecd8feccfcea145c3b
Luca Kanyo <lucakanyo@protonmail.com>
2020-06-11 01:10:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
   end 
   
   def impossible_sides 
-    side1 > (side2 + side3) || side2 > (side1 + side3) || side3 > (side1 + side2)
+    side1 >= (side2 + side3) || side2 >= (side1 + side3) || side3 >= (side1 + side2)
   end 
   
   class TriangleError < StandardError 
